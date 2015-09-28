e9575b01b41dcc1adf9d219264ad4f702aa8a767
Richard <rfree86@gmail.com>
2015-09-28 16:03:21 -0400

    created Likes constructors, functions and eventhandlers to count the #of button clicks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,27 @@
-var models = require('models');
+var Pagebutton = require('models/likes');
 
 $(document).ready(function(){
   $('body').prepend(JST.application());
 
+
+var likebutton = new Pagebutton();
+$('.likes').on('click', function(){
+  likebutton.newlikes();
+
+});
+
+likebutton.on('change:likecount', function(){
+  $('.likes').text(likebutton.get('likecount'));
+});
+
+likebutton.on('change:likecount', function(){
+if(likebutton.get('likecount') < 2) {
+  $('.likes').text(likebutton.get('likecount') + " like");
+}
+  else  {
+    $('.likes').text(likebutton.get('likecount') + " likes");
+  }
+
+});
 
 });
