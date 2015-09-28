590d474fc60e77a6afffb241e38a2b217d1cfed0
Richard <rfree86@gmail.com>
2015-09-28 14:31:01 -0400

    built button in templates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,7 @@
 var models = require('models');
-var views = require('views');
 
 $(document).ready(function(){
   $('body').prepend(JST.application());
 
-  var view = new PostsView();
 
-  $(document).on('posts:fetched', function(event, posts){
-    view.showPosts(posts);
-  });
-
-  Post.fetch();
 });
